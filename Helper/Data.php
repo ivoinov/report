a31a9c67f450639d60c6b65577cef8835509ba53
Ilya Voinov <ilya.voinov@yahoo.com>
2018-03-27 02:02:00 +0300

    Add attachment CSV file

--- Helper/Data.php
+++ Helper/Data.php
@@ -19,9 +19,10 @@
 
 class Ivoinov_Report_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    CONST XML_PATH_ORDER_REPORT_ENABLED  = 'ivoinov_report/order/enabled';
-    CONST XML_PATH_ORDER_REPORT_TEMPLATE = 'ivoinov_report/order/template';
-    CONST XML_PATH_ORDER_REPORT_SEND_TO  = 'ivoinov_report/order/send_to';
+    CONST XML_PATH_ORDER_REPORT_ENABLED            = 'ivoinov_report/order/enabled';
+    CONST XML_PATH_ORDER_REPORT_TEMPLATE           = 'ivoinov_report/order/template';
+    CONST XML_PATH_ORDER_REPORT_SEND_TO            = 'ivoinov_report/order/send_to';
+    CONST XML_PATH_ORDER_REPORT_IS_ATTACH_CSV_FILE = 'ivoinov_report/order/is_attach_csv_file';
 
     /**
      * Send order report
@@ -36,11 +37,15 @@ class Ivoinov_Report_Helper_Data extends Mage_Core_Helper_Abstract
             $template = Mage::getStoreConfig(self::XML_PATH_ORDER_REPORT_TEMPLATE);
             $sendTo = explode(',', Mage::getStoreConfig(self::XML_PATH_ORDER_REPORT_SEND_TO));
             $vars = array('orderList' => $this->_getOrderList());
-            foreach ($sendTo as $sendToEmail) {
+            if (count($sendTo) > 0) {
                 /** @var Mage_Core_Model_Email_Template $emailTemplate */
                 $emailTemplate = Mage::getModel('core/email_template');
+                $emailTemplate->getMail()->addCc($sendTo);
+                if (Mage::getStoreConfigFlag(self::XML_PATH_ORDER_REPORT_IS_ATTACH_CSV_FILE)) {
+                    $this->_attachCSVFile($emailTemplate);
+                }
                 $emailTemplate->sendTransactional($template, array('name' => $senderName, 'email' => $senderEmail),
-                    $sendToEmail, $sendToEmail, $vars);
+                    $sendTo[0], $sendTo[0], $vars);
             }
         }
     }
@@ -68,5 +73,49 @@ class Ivoinov_Report_Helper_Data extends Mage_Core_Helper_Abstract
         }
 
         return $orderList;
+    }
+
+    protected function _attachCSVFile(Mage_Core_Model_Email_Template $emailTemplate)
+    {
+        $fp = fopen('php://temp', 'r+');
+        fputcsv($fp, array('date', 'number_of_orders'));
+        fputcsv($fp, array($this->_getCurrentDate(), $this->_getNumberOfOpenOrders()));
+        rewind($fp);
+        $body = fread($fp, 1048576);
+        fclose($fp);
+        $emailTemplate->getMail()
+            ->createAttachment($body, Zend_Mime::TYPE_OCTETSTREAM, Zend_Mime::DISPOSITION_ATTACHMENT,
+                Zend_Mime::ENCODING_BASE64, 'openOrders.csv');
+    }
+
+    /**
+     * Return current day
+     *
+     * @return string
+     */
+    protected function _getCurrentDate()
+    {
+        return Mage::getModel('core/date')->date('d/m/Y');
+    }
+
+    /**
+     * Return number of opened orders during last day.
+     *
+     * @return integer
+     */
+    protected function _getNumberOfOpenOrders()
+    {
+        /** @var Mage_Core_Model_Date $coreDate */
+        $coreDate = Mage::getModel('core/date');
+        $to = $coreDate->timestamp();
+        $from = strtotime('-1 day', $to);
+        /** @var Mage_Sales_Model_Resource_Order_Collection $ordersCollection */
+        $ordersCollection = Mage::getResourceModel('sales/order_collection');
+        $ordersCollection->addFieldToFilter('created_at', array(
+            'from' => date('Y-m-d H:i:s', $from),
+            'to'   => date('Y-m-d H:i:s', $to),
+        ));
+
+        return count($ordersCollection);
     }
 }
